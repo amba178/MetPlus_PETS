9228f0de6e2e1b4f7fabf1517b21159862fc884d
Salem Amba <salemamba@gmail.com>
2016-01-19 18:05:33 -0800

    removed email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,4 @@
 Rails.application.configure do
-  #   config.action_mailer.raise_delivery_errors = true 
-  # config.action_mailer.perform_deliveries = true
-  # config.serve_static_assets = true
-  # config.assets.initialize_on_precompile = false
-  # config.action_mailer.delivery_method = :smtp 
-  # host = 'evening-cove-51018.herokuapp.com'
-  # config.action_mailer.default_url_options = { host: host }
-
-  # ActionMailer::Base.smtp_settings = {
-  #    :address        => 'smtp.sendgrid.net',
-  #    :port           => '587',
-  #    :authentication => :plain,
-  #    :user_name      => ENV['SENDGRID_USERNAME'],
-  #    :password       => ENV['SENDGRID_PASSWORD'],
-  #    :domain         => 'heroku.com',
-  #    :enable_starttls_auto => true
-  #  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -79,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -90,12 +73,18 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp 
   #this will take care of devise setup default url in production
-  # config.action_mailer.default_url_options = { :host => 'metplus.org' }
+  config.action_mailer.default_url_options = { :host => 'metplus.org' }
 
+  
 end
+
+
+
+
