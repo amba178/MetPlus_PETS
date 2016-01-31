0e200e83b7fc36f249185e96bd4a989219f35cbe
Salem Amba <salemamba@gmail.com>
2016-01-30 16:59:35 -0800

    email setup moved to environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  
 
 
 end 
