ede8277787d057691293e5ece8ded501a5ea1369
Salem Amba <salemamba@gmail.com>
2016-01-30 16:38:06 -0800

    updated production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com",
+      domain: 'sheltered-shore-82235.herokuapp.com'
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
